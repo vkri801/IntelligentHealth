0f052e936e01aaec1c0c155912eaa3d010dc5400
VINCENT-PC\vk104 <vincentk12@xtra.co.nz>
2021-04-05 18:53:31 +1200

    Add project files.

---
+++ MainPage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace Intelligent_Health
+{
+    public partial class MainPage : Form
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead("http://nanostring-public-share.s3-website-us-west-2.amazonaws.com/GeoScriptHub/KidneyDataset/Kidney_Sample_Annotations.txt");
+            StreamReader reader = new StreamReader(stream);
+            String content = reader.ReadToEnd();
+
+            richTextBox1.Text = content;
+           
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
